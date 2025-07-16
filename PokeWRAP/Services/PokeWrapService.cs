442b6c5017f203175073072915236a0cf65280f7
Christian Delpin <chrisakde@gmail.com>
2025-07-15 23:28:55 -0400

    Initialization.

---
+++ PokeWRAP/Services/PokeWrapService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeWRAP.Services
+{
+    public class PokeWrapService
+    {
+
+        private readonly PokeWrapClient _client;
+        public PokeWrapService() 
+        {
+            _client = new PokeWrapClient();
+        }
+    }
+}
