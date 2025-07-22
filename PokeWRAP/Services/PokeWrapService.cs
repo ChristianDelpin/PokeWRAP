40d63e86dce03e944e4f8cb82f9778a0e638a138
Christian Delpin <chrisakde@gmail.com>
2025-07-21 23:43:48 -0400

    basic functionality

--- PokeWRAP/Services/PokeWrapService.cs
+++ PokeWRAP/Services/PokeWrapService.cs
@@ -14,5 +14,46 @@ namespace PokeWRAP.Services
         {
             _client = new PokeWrapClient();
         }
+        public async Task<T> GetAbilityAsync<T>()
+        {
+            return await _client.GetAbilityAsync<T>();
+        }
+        public async Task<T> GetBerryAsync<T>()
+        {
+            return await _client.GetBerryAsync<T>();
+        }
+
+
+        // Copy of what I attempted to do in Finbot.
+        /*
+          public async Task<Result<T>> TryGetAsync<T>(string endpoint) where T : NamedApiResource, new()
+        {
+            try
+            {
+                var resource = await _pokeClient.GetResourceAsync<T>(endpoint);
+                return new Result<T>()
+                {
+                   IsSuccessful = true,
+                   Data = resource
+                };
+            }
+            catch (HttpRequestException e)
+            {
+                return new Result<T>()
+                {
+                    IsSuccessful = false,
+                    ErrorMessage = $"HTTP Request failed: {e.Message}"
+                };
+            }
+            catch (Exception e)
+            {
+                return new Result<T>()
+                {
+                    IsSuccessful = false,
+                    ErrorMessage = $"An error occurred: {e.Message}"
+                };
+            }
+        }
+        */
     }
 }
